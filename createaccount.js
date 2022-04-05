074b1a469c3a866ea8d6b1af2cdb586050907bde
ahernan5 <ahernan5@stevens.edu>
2022-04-05 02:59:26 -0400

    login/styling element changes

---
+++ createaccount.js
@@ -0,0 +1,64 @@
+   
+    function createCookie(key, value, date) {
+        const expiration = new Date(date).toUTCString();
+        const cookie = escape(key) + "=" + escape(value) + ";expires=" + expiration + ";";
+        document.cookie = cookie;
+        console.log(cookie);
+        console.log("Creating new cookie with key: " + key + " value: " + value + " expiration: " + expiration);
+    }
+    
+    
+    window.onload = function() {
+        axios.get('https://sheetdb.io/api/v1/0fjcjli9h840i')
+            .then( response => {
+                var users = response.data;
+                var condition = true;
+                while(condition){
+                    var isid = Math.floor(Math.random() * 100000)
+                    for (i=0; i < users.length; i++){
+                        if(users[i].ISID == isid){
+                            break;
+                        }
+                    }
+                    if(i == users.length){
+                        condition = false;
+                        createCookie('ISID', isid, Date.UTC(2022, 8, 1));
+                    }
+                }
+        });
+    }
+    
+    function readCookie(name) {
+        let key = name + "=";
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1, cookie.length);
+            }
+            if (cookie.indexOf(key) === 0) {
+                return cookie.substring(key.length, cookie.length);
+            }
+        }
+        return null;
+    }
+    
+    function postValues(){
+        var fname = document.getElementById('firstname').value;
+        var lname = document.getElementById('lastname').value;
+        var pword = document.getElementById('password').value;
+        var mail = document.getElementById('Email').value;
+        var uname = document.getElementById('username').value;
+        var maj = document.getElementById('major').value;
+       
+    
+        axios.post('https://sheetdb.io/api/v1/0fjcjli9h840i',{
+            "data": [{"ISID": readCookie('ISID'), "First_Name": fname, "Last_Name": lname, "Password": pword,"Email": mail, "Username": uname, "Major": maj}]
+        }).then( response => {
+            console.log(response.data);
+            window.location.href = 'index.html';
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
---
+++ login.js
@@ -0,0 +1,45 @@
+function cookieAlert(){
+    alert('You need cookies enabled in order to use this website!')
+}
+cookieAlert();
+
+function createCookie(key, value, date) {
+    const expiration = new Date(date).toUTCString();
+    const cookie = escape(key) + "=" + escape(value) + ";expires=" + expiration + ";";
+    document.cookie = cookie;
+    console.log(cookie);
+    console.log("Creating new cookie with key: " + key + " value: " + value + " expiration: " + expiration);
+}
+
+function check_login(){
+    axios.get('https://sheetdb.io/api/v1/0fjcjli9h840i')
+    .then( response => {
+        var users = response.data;
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        for (i=0; i < users.length; i++){
+            if (users[i].Username == username && users[i].Password == password){
+                createCookie('ISID', users[i].ISID, Date.UTC(2022, 8, 1));
+                window.location.href = 'index.html';
+            }
+        }
+        document.getElementById('incorrect').innerHTML = "Incorrect Password or Username";
+        
+    });
+}
+
+
+function readCookie(name) {
+    let key = name + "=";
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(key) === 0) {
+            return cookie.substring(key.length, cookie.length);
+        }
+    }
+    return null;
+}
